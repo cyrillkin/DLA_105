014b585099fd718b292ed4f69d966e982b2fe95e
cyrillkin <vision21@yandex.ru>
2021-03-01 16:14:53 +0400

    01.03

--- Lesson-4/hw04_easy.py
+++ Lesson-4/hw04_easy.py
@@ -3,10 +3,9 @@
 # т.е функция ничего не возвращает, а выводит на консоль ответ самостоятельно
 # Предполагается, что 1км = 1,609 мили
 
-# def convert(km):
-#     miles = 1.609*km
-#     print(miles)
-# convert(15)
+def convert(km):
+    miles = 1.609*km
+    print(miles)
 
 # Задание-2:
 # Напишите функцию, округляющую полученное произвольное десятичное число
@@ -14,62 +13,51 @@
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
+def my_round(number, ndigits):
+    integer = int(number)
+    fraction = number % 1 * 10 ** ndigits
+    balance = float(fraction) - int(fraction)
+    if balance >= 0.5:
+        var = integer + (int(fraction) + 1) / 10 ** ndigits
+        var = '{:.5f}'.format(var)
+        return var
+    else:
+        var = intreger + int(fraction) / 10 ** ndigits
+        var = '{:.5f}'.format(var)
+        return var
 
-# def my_round(number, ndigits):
-#     integer = int(number)
-#     fraction = number % 1 * 10 ** ndigits
-#     balance = float(fraction) - int(fraction)
-#     if balance >= 0.5:
-#         var = integer + (int(fraction) + 1) / 10 ** ndigits
-#         var = '{:.5f}'.format(var)
-#         return var
-#     else:
-#         var = intreger + int(fraction) / 10 ** ndigits
-#         var = '{:.5f}'.format(var)
-#         return var
-#
-# print(my_round(2.1234567, 5))
-# print(my_round(2.1999967, 5))
-# print(my_round(2.9999967, 5))
-#
 # второй вариант
-#
-# def my_round(number, ndigits):
-# # number = 2.64515
-# # ndigits = 0
-#     a = str(number).split('.')
-#
-# # k_1
-#     if int(a[1][0] and ndigits == 0) >= 5:
-#         k_1 = int(a[0])+1
-#     else:
-#         k_1 = a[0]
-#
-# # k_2
-#     len = len(a[1])
-#     if ndigits ==0:
-#         k_2 = ""
-#     elif ndigits < len:
-#         if int(a[1][len-1]) >= 5:
-#             k_2 = int(a[1][0:ndigits])+1
-#         else:
-#             k_2 = a[1][0:ndigits]
-#     elif ndigits == len:
-#         k_2 = a[1][0:ndigits]
-#     else:
-#         k_2 = a[1][0:ndigits] + (ndigits - len) * '0'
-#
-#     if ndigits ==0:
-#         str = str(k_1)
-#     else:
-#         str = str(k_1) + '.' + str(k_2)
-#
-#     return str
-#
-# print(my_round(2.1234567, 5))
-# print(my_round(2.1999967, 5))
-# print(my_round(2.9999967, 5))
 
+def my_round_2(number, ndigits):
+
+    a = (number).split('.')
+
+    # k_1
+    if int(a[1][0]) >= 5:
+        k_1 = int(a[0])+1
+    else:
+        k_1 = str(a[0])
+
+    # k_2
+    lenght = len(a[1])
+    if ndigits == 0:
+        k_2 = ""
+    elif ndigits < lenght:
+        if int(a[1][lenght-1]) >= 5:
+            k_2 = int(a[1][0:ndigits]) + 1
+        else:
+            k_2 = a[1][0:ndigits]
+    elif ndigits == lenght:
+        k_2 = a[1][0:ndigits]
+    else:
+        k_2 = a[1][0:ndigits] + (ndigits - lenght) * '0'
+
+    if ndigits == 0:
+        string = str(k_1)
+    else:
+        string = str(k_1) + '.' + str(k_2)
+
+    return string
 
 # Задание-3:
 # Дан шестизначный номер билета. Определить, является ли билет счастливым.
@@ -78,14 +66,53 @@
 # !!!P.S.: функция не должна НИЧЕГО print'ить, должна возвращать либо True,
 # ибо False (если счастливый и несчастливый соответственно)
 
-# def lucky_ticket(number_ticket):
-#     number = str(number_ticket)
-#     lst1 = int(number[:1]) + int(number[1:2]) + int(number[2:3])
-#     lst2 = int(number[-1]) + int(number[-2]) + int(number[-3])
-#     if lst1 == lst2:
-#         return True
-#     else:
-#         return False
-#
-# ticket = 123123
-# print(lucky_ticket(ticket))
+def lucky_ticket(number_ticket):
+    number = str(number_ticket)
+    lst1 = int(number[:1]) + int(number[1:2]) + int(number[2:3])
+    lst2 = int(number[-1]) + int(number[-2]) + int(number[-3])
+    if lst1 == lst2:
+        return True
+    else:
+        return False
+
+def lucky_ticket_2(number_ticket):
+    number = str(number_ticket)
+    first_part_ticket = number[:3]
+    second_part_ticket = number[3:]
+
+    result_first = 0
+    result_second = 0
+
+    for i in first_part_ticket:
+        result_first += int(i)
+
+    for i in second_part_ticket:
+        result_second += int(i)
+
+    if result_first == result_second:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+
+    convert(10)
+
+    print(my_round(2.1234567, 5))
+    print(my_round(2.1999967, 5))
+    print(my_round(2.9999967, 5))
+
+    print(my_round_2('2.1234567', 5))
+    print(my_round_2('2.1999967', 5))
+    print(my_round_2('2.9999967', 5))
+
+    print(lucky_ticket(123006))
+    print(lucky_ticket(123214))
+    print(lucky_ticket(436751))
+    print(lucky_ticket(500751))
+
+    print(lucky_ticket_2(123006))
+    print(lucky_ticket_2(123214))
+    print(lucky_ticket_2(436751))
+    print(lucky_ticket_2(500751))
+
--- Lesson-6/hw06_easy.py
+++ Lesson-6/hw06_easy.py
@@ -1,9 +1,15 @@
+import os
+import shutil
+import sys
+
 # Задача-1:
 # Следующая программа написана верно, однако содержит места потенциальных ошибок.
 # используя конструкцию try добавьте в код обработку соответствующих исключений.
 # Пример.
 # Исходная программа:
+
 def avg(a, b):
+
     """Вернуть среднее геометрическое чисел 'a' и 'b'.
 
     Параметры:
@@ -12,13 +18,23 @@ def avg(a, b):
     Результат:
         - float.
     """
-    return (a * b) ** 0.5
+    while True:
+        try:
+            a = float(input("a = "))
+            break
+        except ValueError:
+            print('Введите любое число')
+            continue
 
-
-a = float(input("a = "))
-b = float(input("b = "))
-c = avg(a, b)
-print("Среднее геометрическое = {:.2f}".format(c))
+    while True:
+        try:
+            b = float(input("b = "))
+            break
+        except ValueError:
+            print('Введите любое число')
+            continue
+    c = (a * b) ** 0.5
+    print("Среднее геометрическое = {:.2f}".format(c))
 
 # ПРИМЕЧАНИЕ: Для решения задач 2-4 необходимо познакомиться с модулями os, sys!
 # СМ.: https://pythonworld.ru/moduli/modul-os.html, https://pythonworld.ru/moduli/modul-sys.html
@@ -28,8 +44,52 @@ print("Среднее геометрическое = {:.2f}".format(c))
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+def make_dir(dir):
+    dir_path = os.path.join(os.getcwd(), dir)
+    try:
+        os.mkdir(dir_path)
+        print('Папка', dir, 'создана')
+    except FileExistsError:
+        print('Папка', dir, 'уже существует')
+
+def remove_dir(dir):
+    dir_path = os.path.join(os.getcwd(), dir)
+    try:
+        os.rmdir(dir_path)
+        print('Папка', dir, 'удалена')
+    except FileNotFoundError:
+        print('Папки', dir, 'не существует')
+
 # Задача-3:
 # Напишите скрипт, отображающий папки текущей директории.
 
+def current_dir(path):
+    for i in os.listdir(path):
+        print(i)
+
 # Задача-4:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+def copy(new_name):
+    file_name = os.path.basename(sys.argv[0])
+    shutil.copy(file_name, new_name)
+
+if __name__ == '__main__':
+
+ #   avg(10, 20)
+
+    # i = 1
+    # while i <= 9:
+    #     dir = 'dir_' + str(i)
+    #     make_dir(dir)
+    #     i += 1
+
+    # i = 1
+    # while i <= 9:
+    #     dir = 'dir_' + str(i)
+    #     remove_dir(dir)
+    # i += 1
+
+    path = os.getcwd()
+    current_dir(path)
+    copy('hgyu')
