fa713e345f37c2ab82c42ad49aac466d9cb06e74
cyrillkin <vision21@yandex.ru>
2021-03-06 13:20:33 +0400

    6.03

--- Lesson-7/hw07_easy.py
+++ Lesson-7/hw07_easy.py
@@ -1,9 +1,101 @@
+import math
+
 # Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
 
+class Triangle:
+    def __init__(self, dot_1_x, dot_1_y, dot_2_x, dot_2_y, dot_3_x, dot_3_y):
+        self.dot_1_x = dot_1_x
+        self.dot_1_y = dot_1_y
+        self.dot_2_x = dot_2_x
+        self.dot_2_y = dot_2_y
+        self.dot_3_x = dot_3_x
+        self.dot_3_y = dot_3_y
+
+    def square(self):
+        segment_1 = ((self.dot_2_x - self.dot_1_x)**2+(self.dot_2_y - self.dot_1_y)**2)**0.5
+        segment_2 = ((self.dot_3_x - self.dot_2_x) ** 2 + (self.dot_3_y - self.dot_2_y) ** 2) ** 0.5
+        segment_3 = ((self.dot_3_x - self.dot_1_x) ** 2 + (self.dot_3_y - self.dot_1_y) ** 2) ** 0.5
+        p = (segment_1 + segment_2 + segment_3)/2
+        square = (p * (p - segment_1) * (p - segment_2) * (p - segment_3))**0.5
+        print(square)
+
+    def high(self):
+        segment_1 = ((self.dot_2_x - self.dot_1_x) ** 2 + (self.dot_2_y - self.dot_1_y) ** 2) ** 0.5
+        segment_2 = ((self.dot_3_x - self.dot_2_x) ** 2 + (self.dot_3_y - self.dot_2_y) ** 2) ** 0.5
+        segment_3 = ((self.dot_3_x - self.dot_1_x) ** 2 + (self.dot_3_y - self.dot_1_y) ** 2) ** 0.5
+        p = (segment_1 + segment_2 + segment_3) / 2
+        h = (2/segment_1) * (p * (p - segment_1) * (p - segment_2) * (p - segment_3)) ** 0.5
+        print(h)
+
+    def perimetr(self):
+        segment_1 = ((self.dot_2_x - self.dot_1_x) ** 2 + (self.dot_2_y - self.dot_1_y) ** 2) ** 0.5
+        segment_2 = ((self.dot_3_x - self.dot_2_x) ** 2 + (self.dot_3_y - self.dot_2_y) ** 2) ** 0.5
+        segment_3 = ((self.dot_3_x - self.dot_1_x) ** 2 + (self.dot_3_y - self.dot_1_y) ** 2) ** 0.5
+        perimetr = segment_1 + segment_2 + segment_3
+        print(perimetr)
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
 # Предусмотреть в классе методы:
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
 
+class trapezoid:
+    def __init__(self, dot_1_x, dot_1_y, dot_2_x, dot_2_y, dot_3_x, dot_3_y, dot_4_x, dot_4_y):
+        self.dot_1_x = dot_1_x
+        self.dot_1_y = dot_1_y
+        self.dot_2_x = dot_2_x
+        self.dot_2_y = dot_2_y
+        self.dot_3_x = dot_3_x
+        self.dot_3_y = dot_3_y
+        self.dot_4_x = dot_4_x
+        self.dot_4_y = dot_4_y
+
+    # def segment(self, x_1, y_1, x_2, y_2):
+    #     len = ((x_2 - x_1) ** 2 + ((y_2 - y_1)) ** 2) ** 0.5
+    #     return len
+
+    def check_trapezoid(self):
+        diagonal_1 = ((self.dot_3_x - self.dot_1_x) ** 2 + (self.dot_3_y - self.dot_1_y) ** 2) ** 0.5
+        diagonal_2 = ((self.dot_4_x - self.dot_2_x) ** 2 + (self.dot_4_y - self.dot_2_y) ** 2) ** 0.5
+        if diagonal_1 == diagonal_2:
+            print('Trapezia ravnobochaya')
+        else:
+            print('Trapezia neravnobochaya')
+
+    def perimetr(self):
+        segment_1 = ((self.dot_2_x - self.dot_1_x) ** 2 + ((self.dot_2_y - self.dot_1_y)) ** 2) ** 0.5
+        segment_2 = ((self.dot_3_x - self.dot_2_x) ** 2 + ((self.dot_3_y - self.dot_2_y)) ** 2) ** 0.5
+        segment_3 = ((self.dot_4_x - self.dot_3_x) ** 2 + ((self.dot_4_y - self.dot_3_y)) ** 2) ** 0.5
+        segment_4 = ((self.dot_4_x - self.dot_1_x) ** 2 + ((self.dot_4_y - self.dot_1_y)) ** 2) ** 0.5
+        perimetr = segment_1 + segment_2 + segment_3 + segment_4
+        print(perimetr)
+
+    def square(self):
+        a = ((self.dot_3_x - self.dot_2_x) ** 2 + ((self.dot_3_y - self.dot_2_y)) ** 2) ** 0.5
+        b = ((self.dot_4_x - self.dot_1_x) ** 2 + ((self.dot_4_y - self.dot_1_y)) ** 2) ** 0.5
+        h = self.dot_2_y - self.dot_1_y
+        square = 0.5 * h * (a + b)
+        print(square)
+
+    def len(self):
+        len_1 = ((self.dot_2_x - self.dot_1_x) ** 2 + (self.dot_2_y - self.dot_1_y) ** 2) ** 0.5
+        len_2 = ((self.dot_3_x - self.dot_2_x) ** 2 + (self.dot_3_y - self.dot_2_y) ** 2) ** 0.5
+        len_3 = ((self.dot_4_x - self.dot_3_x) ** 2 + (self.dot_4_y - self.dot_3_y) ** 2) ** 0.5
+        len_4 = ((self.dot_4_x - self.dot_1_x) ** 2 + (self.dot_4_y - self.dot_1_y) ** 2) ** 0.5
+        print(len_1, ',', len_2, ',', len_3, ',', len_4)
+
+if __name__ == '__main__':
+
+    figure = Triangle(5, 3, 8, 3, 8, 6)
+
+    figure.square()
+    figure.high()
+    figure.perimetr()
+
+    figure = trapezoid(1, 4, 2, 6, 4, 6, 5, 4)
+
+    figure.check_trapezoid()
+    figure.perimetr()
+    figure.square()
+    figure.len()
--- Lesson-7/hw07_normal.py
+++ Lesson-7/hw07_normal.py
@@ -15,3 +15,87 @@
 #  (Ученик --> Класс --> Учителя --> Предметы)
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
+
+class Person:
+    def __init__(self, full_name):
+        self.full_name = full_name
+
+    def get_full_name(self):
+        return self.full_name
+
+class Student(Person):
+    def __init__(self, full_name, first_parent, second_parent, school_class):
+        Person.__init__(self, full_name)
+        self.first_parent = first_parent
+        self.second_parent = second_parent
+        self.school_class = school_class
+
+    def get_class_room(self):
+        return self.school_class
+
+    def get_parent(self):
+        return self.first_parent, self.second_parent
+
+class Teacher(Person):
+    def __init__(self, full_name, classes, subject):
+        Person.__init__(self, full_name)
+        self.classes = classes
+        self.subject = subject
+
+    def get_subject(self):
+        return self.subject
+
+    def get_classes(self):
+        return self.classes
+
+if __name__ == '__main__':
+
+    #classes
+    school_classes = ['9A', '10A', '11B']
+
+    #students
+    students = [Student('Иванов И.И.', 'Иван Иванович', 'Анна Ивановна', school_classes[0]), Student('Петров П.П.', 'Петр Иванович', 'Юлия Ивановна', school_classes[1]), Student('Сидоров С.С.', 'Семён Иванович', 'Мария Ивановна', school_classes[2])]
+
+    #parents
+    parent_1 = ['Иван Иванович', 'Анна Ивановна']
+    parent_2 = ['Петр Иванович', 'Юлия Ивановна']
+    parent_3 = ['Семён Иванович', 'Мария Ивановна']
+
+    #teachers
+    teachers = [Teacher('Семёнов В.И', ['9A', '10A'], 'Math'), Teacher('Борисов С.А', ['9A', '11B'], 'English'), Teacher('Попов К.У', ['10A', '11B'], 'History')]
+
+    # 1. Получить полный список всех классов школы
+    for i in students:
+        print(i.get_class_room())
+
+    # 2. Получить список всех учеников в указанном классе
+    # (каждый ученик отображается в формате "Фамилия И.О.")
+    name_class = '11A'
+    for i in students:
+        if i.get_class_room() == name_class:
+            print('\n' + i.get_full_name())
+
+    # 3. Получить список всех предметов указанного ученика
+    # (Ученик --> Класс --> Учителя --> Предметы)
+
+    name_student = students[0]
+    for i in teachers:
+        name_class = i.get_classes()
+
+    for name_class in teachers:
+        name_teacher = i.get_full_name()
+
+    for name_teacher in teachers:
+        name_subject = name_teacher.get_subject()
+
+    print(name_student.get_full_name() + ' --> ' + name_student.get_class_room() + ' --> ' + name_teacher.get_full_name() + ' --> ' + name_teacher.get_subject())
+
+    # 4. Узнать ФИО родителей указанного ученика
+    parents = name_student.get_parent()
+    print(parents)
+
+    # 5. Получить список всех Учителей, преподающих в указанном классе
+    name_class = '10A'
+    for i in teachers:
+        if name_class in i.get_classes():
+            print(i.get_full_name())
