d61fb4efa9863216e7afb21bde2d810359ce876d
cyrillkin <vision21@yandex.ru>
2021-03-24 17:08:47 +0400

    24.03

---
+++ Lesson-10/hw10.py
@@ -0,0 +1,25 @@
+# Предметная область – магазин.
+# Разработать класс Shop, описывающий работу магазина продуктов.
+# Разработать класс Products, продукт описывается следующими параметрами:
+# уникальный идентификатор, название продукта, стоимость, количество.
+# Разработать класс FruitProduct на базе класс Product, фрукт характеризуется параметрами:
+# страна изготовителя, срок годности.
+
+class Shop:
+    pass
+
+class Products:
+    def __init__(self, id_product, name_product, price, count):
+        self.id_product = id_product
+        self.name_product = name_product
+        self.price = price
+        self.count = count
+
+        pass
+
+class FruitProduct(Product):
+    def __init__(self, made_country, expiration):
+        self.made_country = made_country
+        self.expiration = expiration
+
+    pass
--- Lesson-7/hw07_hard.py
+++ Lesson-7/hw07_hard.py
@@ -9,4 +9,58 @@
 
 # С использованием классов.
 # Реализуйте классы сотрудников так, чтобы на вход функции-конструктора
-# каждый работник получал строку из файла
+# каждый- работник получал строку из файла
+
+class Worker:
+    def __init__(self, name, surname, salary, dolznost, norma):
+        self.name = name
+        self.surname = surname
+        self.salary = int(salary)
+        self.dolznost = dolznost
+        self.norma = int(norma)
+        self.hours = 0
+
+    def work_hours(self):
+        with open('data/hours.txt', 'r', encoding='UTF-8') as f:
+            for i in f.readlines():
+                if i.split()[0] == self.name and i.split()[1] == self.surname:
+                    self.hours = int(i.split()[2])
+                    break
+                else:
+                    continue
+
+    def calc(self):
+        for_hour = self.salary // self.norma
+        salary = 0
+        if self.hours > self.norma:
+            razn = self.hours - self.norma
+            salary = (razn * for_hour) * 2
+            return (salary + self.salary)
+        elif self.hours < self.norma:
+            razn = self.norma - self.hours
+            salary = razn * for_hour
+            return (self.salary - salary)
+        else:
+            return (self.salary)
+
+    def write(self, salary):
+        with open('salary.txt', 'a', encoding='UTF-8') as f:
+            f.write(self.name + ' ' + self.surname + ' ' + str(salary) + ' ' + self.dolznost)
+            f.write('\n')
+
+
+if __name__ == '__main__':
+    def total(file):
+        f = open(file, 'r', encoding='UTF-8')
+        for i in f.readlines():
+            if i.count('Имя') == 1:
+                continue
+            else:
+                name, surname, salary, position, norm_hours = i.split()
+                workman = Worker(name, surname, salary, position, norm_hours)
+                workman.work_hours()
+                salary = workman.calc()
+                workman.write(salary)
+        f.close()
+
+total('data/workers.txt')
